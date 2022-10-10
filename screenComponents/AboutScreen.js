5c2f241adc9b58b24ab909b3a3ca955518eb8dd6
Tyler Voss <tylervoss101@gmail.com>
2022-10-10 19:54:32 -0400

    about screen stack

--- screenComponents/AboutScreen.js
+++ screenComponents/AboutScreen.js
@@ -83,6 +83,8 @@ export default function AboutScreen({ route }) {
                 </TouchableOpacity>
             </View>
         </View>
+
+
     )
 }
 
--- screenComponents/AboutScreenStack.js
+++ screenComponents/AboutScreenStack.js
@@ -1,17 +1,20 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './HomeScreen';
 import AboutScreen from './AboutScreen';
+import whiteLogo from '../assets/whiteLogo.png';
+import DisplayScreen from './DisplayScreen';
+//import DisplayScreen from './DisplayScreen.js;'
 
 const Stack = createNativeStackNavigator();
 
-export default function HomeScreenStack() {
+export default function AboutScreenStack() {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
             <Stack.Screen name="About" component={AboutScreen} options={{headerShown: false}} />
+            <Stack.Screen name="Display" component={DisplayScreen} options={{headerShown: false}} />
         </Stack.Navigator>
     )
 }
