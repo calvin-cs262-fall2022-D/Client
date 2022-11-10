e1a73e1576eb161a601c76bdfb0d2c34bf6d5a83
Tyler Voss <tylervoss101@gmail.com>
2022-11-09 20:53:36 -0500

    adding json data from heroku

--- components/Semester.js
+++ components/Semester.js
@@ -1,53 +1,82 @@
-import Movie from './Movie';
-import { StyleSheet, Text, View, SafeAreaView, FlatList, } from 'react-native';
+import Movie from "./Movie";
+import { StyleSheet, Text, View, SafeAreaView, FlatList } from "react-native";
+import { useEffect, useState } from "react";
 
 export default function Semester(props) {
-    // array of objects 
-    const movieData = props.movieData;
+  const [isLoading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    try {
+      // We will fetch data from our Knightflix web service, and store the items in movies
+      const response = await fetch(
+        "https://knightflix-service.herokuapp.com/movies"
+      );
+      const json = await response.json();
+      setMovies(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    const renderMovies = ({ item }) => (
-        <Movie poster={item.image} title={item.title} videoId={item.videoId} description={item.description} course={item.course} />
-    )
+  useEffect(() => {
+    getMovies();
+  }, []);
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.sectionHeading}>
-                <Text style={styles.sectionHeadingText}>{props.text}</Text>
-            </View>
-            <SafeAreaView style={styles.movieList}>
-                <FlatList 
-                    data={movieData}
-                    renderItem={renderMovies}
-                    keyExtractor={item => item.id}
-                    horizontal={true}
-                />
-            </SafeAreaView>
-        </View>
-    )
+  // array of objects
+  const movieData = props.movieData;
+
+  const renderMovies = ({ item }) => (
+    //now we just need to refactor the code so we can access the correct attributes
+    <Movie
+      poster={item.image}
+      title={item.title}
+      videoId={item.videoId}
+      description={item.description}
+      course={item.course}
+    />
+  );
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.sectionHeading}>
+        <Text style={styles.sectionHeadingText}>{props.text}</Text>
+      </View>
+      <SafeAreaView style={styles.movieList}>
+        <FlatList
+          data={movieData}
+          renderItem={renderMovies}
+          keyExtractor={(item) => item.id}
+          horizontal={true}
+        />
+      </SafeAreaView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        height: 400,
-        backgroundColor: "12345",
-    },
-    sectionHeading: {
-        flex: 1,
-        backgroundColor: "#141414",
-        justifyContent: "center",
-        paddingLeft: 30,
-    },
-    sectionHeadingText: {
-        fontSize: 25,
-        fontWeight: "bold",
-        color: "#ECC409",
-        fontFamily: "Fjalla"
-    },
-    movieList: {
-        flex: 6,
-        backgroundColor: "#141414",
-        flexDirection: "row",
-        alignItems: "center",
-        paddingLeft: 15,
-    }
-})
+  container: {
+    height: 400,
+    backgroundColor: "12345",
+  },
+  sectionHeading: {
+    flex: 1,
+    backgroundColor: "#141414",
+    justifyContent: "center",
+    paddingLeft: 30,
+  },
+  sectionHeadingText: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "#ECC409",
+    fontFamily: "Fjalla",
+  },
+  movieList: {
+    flex: 6,
+    backgroundColor: "#141414",
+    flexDirection: "row",
+    alignItems: "center",
+    paddingLeft: 15,
+  },
+});
